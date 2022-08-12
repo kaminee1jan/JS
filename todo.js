fa91f3cc1c7dc855b5182114957dcfe84e7cde8a
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-08-12 14:24:39 +0530

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,42 +1,55 @@
-function deletetask(index) {
-    var list = JSON.parse(localStorage.getItem("keys"));
-    list.splice(index, 1);
-    localStorage.setItem("keys", JSON.stringify(list));
-    ShowTask();
-}
+// function deletetask(index) {
+//     var list = JSON.parse(localStorage.getItem("keys"));
+//     list.splice(index, 1);
+//     localStorage.setItem("keys", JSON.stringify(list));
+//     ShowTask();
+// }
 
-function ShowTask() {
-    var local = JSON.parse(localStorage.getItem("keys"))
-    li = ""
-    index = 0
-    for (j of local) {
-        li += "<li>" + j + "<input type='button' value='delete' onclick='deletetask(" + index + ")'/> </li>"
-        index++
-    }
-    document.getElementById("todoList").innerHTML = li
-    var to_show = "you have" + index + "tasks"
-    document.getElementById("footer").innerHTML = to_show;
-}
-function clearList( ) {
-    localStorage.removeItem("keys");
-    document.getElementById("todoList").innerHTML=" ";
-    document.getElementById("footer").innerHTML=" ";
+// function ShowTask() {
+//     var local = JSON.parse(localStorage.getItem("keys"))
+//     li = ""
+//     index = 0
+//     for (j of local) {
+//         li += "<li>" + j + "<input type='button' value='delete' onclick='deletetask(" + index + ")'/> </li>"
+//         index++
+//     }
+//     document.getElementById("todoList").innerHTML = li
+//     var to_show = "you have" + index + "tasks"
+//     document.getElementById("footer").innerHTML = to_show;
+// }
+// function clearList( ) {
+//     localStorage.removeItem("keys");
+//     document.getElementById("todoList").innerHTML=" ";
+//     document.getElementById("footer").innerHTML=" ";
     
 
-}
+// }
 
-function addToList() {
-    var DocumentGet = document.getElementById("input1");
-    var loc = localStorage.getItem("keys");
-    list = []
-    if (loc == null) {
-        list.push(DocumentGet.value)
-        localStorage.setItem("keys", JSON.stringify(list))
-    } else {
-        var storage = JSON.parse(loc);
-        storage.push(DocumentGet.value)
-        localStorage.setItem("keys", JSON.stringify(storage))
-    }
-    DocumentGet.value = " "
-    ShowTask()
-}
+// function addToList() {
+//     var DocumentGet = document.getElementById("input1");
+//     var loc = localStorage.getItem("keys");
+//     list = []
+//     if (loc == null) {
+//         list.push(DocumentGet.value)
+//         localStorage.setItem("keys", JSON.stringify(list))
+//     } else {
+//         var storage = JSON.parse(loc);
+//         storage.push(DocumentGet.value)
+//         localStorage.setItem("keys", JSON.stringify(storage))
+//     }
+//     DocumentGet.value = " "
+//     ShowTask()
+// }
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+    a=5+"9";
+    document.write(a);
+</script>
